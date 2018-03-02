29855fe8446e42996099a1b6e0491a4ee6fed076
Dmitry <dmitilyin@gmail.com>
2018-03-02 19:13:55 +0300

    Добавлены методы сложения, вычитания, умножения и деления дроби с целым числом

--- src/ch3/TestFraction.java
+++ src/ch3/TestFraction.java
@@ -6,10 +6,12 @@ public class TestFraction {
         Fraction fr2 = new Fraction(-3,30);
 
         System.out.println("fr1=" + fr1.showFraction() + " fr2=" + fr2.showFraction());
+
         System.out.println(fr1.showFraction() + " + " + fr2.showFraction() + " = " + Fraction.summFraction(fr1,fr2).showFraction());
         System.out.println(fr1.showFraction() + " - " + fr2.showFraction() + " = " + Fraction.minusFraction(fr1,fr2).showFraction());
         System.out.println(fr1.showFraction() + " * " + fr2.showFraction() + " = " + Fraction.multiplyFraction(fr1,fr2).showFraction());
         System.out.println(fr1.showFraction() + " / " + fr2.showFraction() + " = " + Fraction.divisionFraction(fr1,fr2).showFraction());
+
         System.out.println(fr1.showFraction() + " + " + 7 + " = " + Fraction.summFraction(fr1,7).showFraction());
         System.out.println(fr1.showFraction() + " - " + 14 + " = " + Fraction.minusFraction(fr1,14).showFraction());
         System.out.println(fr1.showFraction() + " * " + 24 + " = " + Fraction.multiplyFraction(fr1,18).showFraction());
